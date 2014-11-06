fbf0ad4f58c89c784ff7c08419893569fd5876fb
deepak-jain <deepak.jain@jabong.com>
2014-11-06 21:46:58 +0530

    fixed untracked files

--- custom/modules/Cases/Ext/Vardefs/vardefs.ext.php
+++
@@ -1,9 +0,0 @@
-<?php 
- //WARNING: The contents of this file are auto-generated
-
-
- // created: 2014-08-18 14:14:18
-$dictionary['Case']['fields']['subject_type_c']['labelValue']='Subject Type';
-
- 
-?>
--- custom/modules/Contacts/Ext/Vardefs/vardefs.ext.php
+++
@@ -1,9 +0,0 @@
-<?php 
- //WARNING: The contents of this file are auto-generated
-
-
- // created: 2014-09-12 12:37:43
-$dictionary['Contact']['fields']['account_subtype_c']['labelValue']='Account SubType';
-
- 
-?>
