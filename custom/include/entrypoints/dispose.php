bed06a860d14cc243599cf7a7f464716bf133547
deepak-jain <deepak.jain@jabong.com>
2014-11-10 16:38:09 +0530

    phase1 changes in dispose.php

--- custom/include/entrypoints/dispose.php
+++ custom/include/entrypoints/dispose.php
@@ -1,23 +1,6 @@
 <?php
 
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
-if (!validateSession()) {
-	exit('SessionId is not valid.');
-}
-$userId = isset($_REQUEST['userId']) ? $_REQUEST['userId'] : null;
-if (!$userId) {
-	exit('UserId not valid.');
-}
-
-$GLOBALS['customUserId'] = $userId;
-$authentication = new AuthenticationController();
-
-if(empty($_SESSION['authenticated_user_id']) && $authentication->login($userId, '')) {
-	$_SESSION['customUserId'] = $userId;
-} else if($userId != $_SESSION['customUserId']){
-	session_destroy();
-	header('location:' . $_SERVER['REQUEST_URI']);
-}
 
 $crtObjectId = $_REQUEST['crtObjectId'];
 
@@ -36,8 +19,5 @@ if ($result->num_rows > 0) {
 	$call->duration_minutes = $calTime['min'];
 	
 	$beanId = $call->save();
-	//unset($_SESSION['mCallVars_' . $crtObjectId]);
-	$redirectUrl = 'index.php?module=Calls&action=EditView&record=' . $beanId;
-	header('location:' . $redirectUrl);
 }
 exit;
